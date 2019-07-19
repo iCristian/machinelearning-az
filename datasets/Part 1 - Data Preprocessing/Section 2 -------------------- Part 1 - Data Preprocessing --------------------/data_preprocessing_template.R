28822364c05ef1281a78dfe22ae8113c428ac8de
Cristian Carreño León <icristian.mac@gmail.com>
2019-07-19 15:15:11 -0400

    changes

--- datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
+++ datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
@@ -7,7 +7,10 @@ dataset = read.csv('Data.csv')
 # install.packages("caTools")
 library(caTools)
 set.seed(123)
+# Split genera un vector en base a la variable dataset$Purchased con un ratio de 0.8 en la separación de los datos
+# En el vector todos los datos TRUE corresponden a los pertenecientes al ratio y FALSE los que deben quedar afuera
 split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+# subset divide la BD según el vector split definido anteriormente
 training_set = subset(dataset, split == TRUE)
 testing_set = subset(dataset, split == FALSE)
 
