fe821e4c30f802686198e271a3b2e2f5f5c7b28b
Cristian Carreño León <icristian.mac@gmail.com>
2019-07-19 16:13:48 -0400

    notacion de ayuda incorporada

--- datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -15,7 +15,10 @@ import pandas as pd
 
 # Importar el data set
 dataset = pd.read_csv('Data.csv')
+# iloc = index location [filas, columnas] -1 no considera la ultima columna
 X = dataset.iloc[:, :-1].values
+
+# En python un indice comienza siempre de 0 y no toma el ultimo elemento por lo que al indicar 3 selecciona columnas 0, 1 y 2
 y = dataset.iloc[:, 3].values
 
 
@@ -24,8 +27,8 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
-# Escalado de variables
-"""from sklearn.preprocessing import StandardScaler
+# Escalado de variables importante cuando las columnas difieren mucho entre sus vALORES
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)"""
+X_test = sc_X.transform(X_test)
